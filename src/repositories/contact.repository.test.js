5cf3cd71d4954ca65df59d4b83a6a3421b1c59c5
Jesse Hall <jessehall3@gmail.com>
2018-12-08 16:46:25 -0500

    Now using sinon.
    First couple of tests for the contact repository working.

--- src/repositories/contact.repository.test.js
+++ src/repositories/contact.repository.test.js
@@ -1,4 +1,5 @@
 const expect = require('chai').expect;
+const sinon = require('sinon');
 const { ContactRepository } = require('./contact.repository');
 
 describe('Contacts Repository', () => {
@@ -18,6 +19,12 @@ describe('Contacts Repository', () => {
     { id: '3', name: 'Darth Vadar' }
   ];
 
+  const createAwsRequest = (data = null, resolveOrReject = true, errMsg = 'error') => {
+    return {
+      promise: () => resolveOrReject ? Promise.resolve(data) : Promise.reject(new Error('error'))
+    };
+  };
+
   /** @type {ContactsRepository} */
   let respository;
 
@@ -25,30 +32,37 @@ describe('Contacts Repository', () => {
     respository = new ContactRepository(mockDocClient);
   });
 
-  it('should construct a new respository', () => {
+  afterEach(() => {
+    sinon.restore();
+  })
 
+
+  it('should construct a new respository', () => {
     expect(respository).to.exist;
   });
-  //
-  // it('should list contacts', async () => {
-  //   const expectedResult = {
-  //     Items: mockContacts.slice()
-  //   };
-  //
-  //   spyOn(mockDocClient, 'scan').and.returnValues(createAwsRequest(expectedResult), createAwsRequest({ Items: null }));
-  //
-  //   const awsParams = {
-  //     TableName: 'contacts'
-  //   };
-  //
-  //   const results = await respository.list();
-  //
-  //   expect(results).toEqual(expectedResult.Items);
-  //   expect(results.length).toBe(3);
-  //   expect(mockDocClient.scan).toHaveBeenCalledWith(awsParams);
-  //
-  //   const emptyResults = await respository.list();
-  //
-  //   expect(emptyResults).toEqual([]);
-  // });
+
+  it('should list contacts', async () => {
+    const expectedResult = {
+      Items: mockContacts
+    };
+
+    const dockClientStub = sinon.stub(mockDocClient, 'scan');
+    dockClientStub.onCall(0).returns(createAwsRequest(expectedResult));
+    dockClientStub.onCall(1).returns(createAwsRequest({ Items: null }));
+
+    const awsParams = {
+      TableName: 'contacts'
+    };
+
+    const results = await respository.list();
+
+    expect(results).to.equal(expectedResult.Items);
+    expect(results.length).to.equal(3);
+    sinon.assert.calledWith(dockClientStub, awsParams)
+
+    const emptyResults = await respository.list();
+
+    expect(emptyResults).to.be.empty;
+  });
+
 });
